6c622282d1d76d8450d92f3526ca97a1c980b9fc
richardalbani <richardalbani2@gmail.com>
2025-06-04 15:18:58 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const Database = require("better-sqlite3");
+const Database = require("sqlite3");
 const path = require("path");
 
 const app = express();
